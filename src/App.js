7c48bf5f95d595b3b21c3e09a9a7983fc78daf38
Cristopher Garcia <garcacristopher3@gmail.com>
2025-04-13 22:04:47 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+    <div className='introduction'>
+      <div className='presentacion'>
+        <h1><strong>Vectores</strong></h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Los vectores son segmentos de una línea recta que están orientados dentro de un plano bidimensional o tridimensional,
+          también conocido como un espacio vectorial. Su expresión matemática se representa mediante una letra con una flecha en
+          la parte superior y, a nivel gráfico,también se utiliza el recurso de la fecha para señalarlos.
+          Los vectores pueden representar magnitudes físicas con intensidad y dirección, como la fuerza, el desplazamiento y la velocidad.
+          Además, suelen representarse en planos a través de coordenadas.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className='caracteristicas'>
+        <h1><strong>Caracteristicas de los vectores</strong></h1>
+        <p>
+          <strong>Sentido:</strong> viene representado por la punta de la flecha que se expresa gráficamente, 
+          indicando el lugar hacia el cual se dirige el vector.
+        </p>
+        <p>
+          <strong>Dirección:</strong> es la recta sostronge la que se plantea el vector, la cual es continua e infinita en el espacio.
+        </p>
+        <p>
+          <strong>Módulo:</strong> se trata de la longitud entre el inicio y fin del vector, es decir, dónde empieza y dónde termina la flecha.
+        </p>
+        <p>
+          <strong>Amplitud:</strong> es la expresión numérica de la longitud gráfica del vector.
+        </p>
+      </div>
     </div>
+    </>
   );
 }
 
